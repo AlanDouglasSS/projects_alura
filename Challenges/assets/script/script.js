bb443c57e23e9e796bb9b71f883f284b49a203ef
Alan Santos <alandouglasbrowm@gmail.com>
2023-05-29 21:14:22 -0300

    first commit

---
+++ Challenges/assets/script/script.js
@@ -0,0 +1,54 @@
+// Seleciona os elementos HTML
+const textInput = document.getElementById('text-input');
+const encodeButton = document.getElementById('encode-button');
+const decodeButton = document.getElementById('decode-button');
+const messageContainer = document.getElementById('message-container');
+const message = document.getElementById('message');
+const noMessage = document.getElementById('no-message');
+const copyButton = document.getElementById('copy-button');
+
+// Função para codificar a mensagem
+function encodeMessage() {
+    const encodedMessage = btoa(textInput.value);
+    message.textContent = encodedMessage;
+    messageContainer.style.display = 'flex';
+    noMessage.style.display = 'none';
+}
+
+// Função para decodificar a mensagem
+/*
+function decodeMessage() {
+    messageContainer.style.display = 'none';
+    message.textContent = '';
+    noMessage.style.display = 'flex';
+}
+*/
+
+function decodeMessage() {
+    textInput.value = '';   
+    message.textContent = '';
+    reset()
+    /*
+    messageContainer.style.display = 'none';
+    
+    
+    */
+}
+
+function reset(){
+    noMessage.style.display = 'block';
+}
+
+
+// Função para copiar a mensagem codificada
+function copyMessage() {
+    const encodedMessage = message.textContent;
+    navigator.clipboard.writeText(encodedMessage).then(() => {
+        alert('Mensagem copiada!');
+    });
+}
+
+// Adiciona os event listeners aos botões
+encodeButton.addEventListener('click', encodeMessage);
+decodeButton.addEventListener('click', decodeMessage);
+copyButton.addEventListener('click', copyMessage);
